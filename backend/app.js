e1f3fdf6afaa4566e318e7158201b17882819f08
geraldcraig <geraldcraig@me.com>
2026-04-24 18:02:10 +0100

    removing secrets

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -27,7 +28,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect('mongodb+srv://geraldcraig_db_user:ub4zn8i3AVAZkBmF@cluster0.z6lnfir.mongodb.net/mern?appName=Cluster0')
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     app.listen(4000);
   })
--- backend/util/location.js
+++ backend/util/location.js
@@ -2,7 +2,9 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = 'AIzaSyCfDhju70dVF0gF7SgrpJiXyzXKkDCsc1g';
+require('dotenv').config();
+
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 async function getCoordsForAddress(address) {
   // return {
