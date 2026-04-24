c78a9c8ee3f1cc1b6e6a494745363aae964e112b
geraldcraig <geraldcraig@me.com>
2026-04-24 17:44:47 +0100

    backend added

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+mongoose
+  .connect('mongodb+srv://geraldcraig_db_user:ub4zn8i3AVAZkBmF@cluster0.z6lnfir.mongodb.net/mern?appName=Cluster0')
+  .then(() => {
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
